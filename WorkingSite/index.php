522bec4fd61fd613d79122edf3cd36723f830c55
KoenHaemels <koenhaemels@gmail.com>
2017-04-14 10:44:22 +0200

    Add files via upload

--- WorkingSite/index.php
+++ WorkingSite/index.php
@@ -11,31 +11,29 @@
         <meta charset="utf-8" />
 
         <title>Demo Applicatie</title>
-        
+
         <!-- Our CSS stylesheet file -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-        
-        <!--[if lt IE 9]>
+        <link rel="stylesheet" href="css/main.css" />
+
           <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
     </head>
-    
+
     <body>
 <div class="container">
   <h1>Demo Applicatie</h1>
-  <p>Hostname: <?php echo gethostname(); ?></p>
-  <p>IP-address: <?php require 'includes/getip.php'; ?></p>
-  <p> NIEUW </p>	
-</div>
+  <h3>Hostname: <?php echo gethostname(); ?></h3>
+  <h4>IP-address: <?php require 'includes/getip.php'; ?></h4>
 
-<div class="container">
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+<label>
+    <input type="file" name="fileToUpload">
+</label>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 </div>
-	
-          
+
     </body>
 </html>
