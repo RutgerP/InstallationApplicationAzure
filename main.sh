f3cca4db6c7f28b200be501ade9f76ea59ea562a
KoenHaemels <koenhaemels@gmail.com>
2017-04-25 14:52:13 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,13 +11,6 @@ sudo rm -f /var/www/html/* -r
 sudo cp /var/InstallationApplication/WorkingSite/* /var/www/html/ -r -f
 clear
 
-#Installation Curl
-sudo apt-get install curl -y
-#Add the gcsfuse distribution URL as a package source and import its public key:
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 #Update the list of packages available and install cifs-utils.
 sudo apt-get update
 sudo apt-get install cifs-utils -y
@@ -28,20 +21,20 @@ sudo mkdir /var/InstallationApplication
 sudo mkdir /var/InstallationApplication/bucket
 sudo chmod 757 /var/InstallationApplication/bucket
 sudo chmod 777 /etc/fstab
-sudo echo "//testcvostorage.file.core.windows.net/testcvoshare var/InstallationApplication/bucket -o vers=3.0,username=testcvostorage,password=c2q52SLd4BUqaOFGzeGAFWLHfp65RPMQ+MnnFX5Z9Yd0DWtT3du/0VYI8onj/ZIGPmkoGySMWn5Hbs2nSYt81A==,dir_mode=0777,file_mode=0777" >> /etc/fstab
+sudo echo "//stagecvostorage.file.core.windows.net/stagecvofileshare var/InstallationApplication/bucket -o vers=3.0,username=stagecvostorage,password=+OXKp2KtfrrA8zMz03azjasOo1rHh2bZrA4+kyeL4q0LuVLiiPzE+xU7gSoAVyf6jX9vlq/zDATmy1VKWFVWAg==,dir_mode=0777,file_mode=0777" >> /etc/fstab
 clear
 
 #Mount bucket at startup
-sudo chmod 777 /etc/rc.local
-sudo sed -i '$ d' /etc/rc.local
-sudo echo "mount /var/InstallationApplication/bucket" >> /etc/rc.local
-sudo echo "exit 0" >> /etc/rc.local
+#sudo chmod 777 /etc/rc.local
+#sudo sed -i '$ d' /etc/rc.local
+#sudo echo "mount /var/InstallationApplication/bucket" >> /etc/rc.local
+#sudo echo "exit 0" >> /etc/rc.local
 
-mount /var/InstallationApplication/bucket
+#mount /var/InstallationApplication/bucket
 
-sudo chmod +x /var/InstallationApplication/Tests/test.sh
-sudo chmod 777 /var/InstallationApplication/Tests/test.sh
-sudo chmod 777 /var/InstallationApplication/Tests/configyamllint.yml
-/var/InstallationApplication/Tests/test.sh
+#sudo chmod +x /var/InstallationApplication/Tests/test.sh
+#sudo chmod 777 /var/InstallationApplication/Tests/test.sh
+#sudo chmod 777 /var/InstallationApplication/Tests/configyamllint.yml
+#/var/InstallationApplication/Tests/test.sh
 
 echo "Done"
